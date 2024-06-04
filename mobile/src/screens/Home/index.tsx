2ae1a043cadeade9d4174846b19bd7f35924e7a3
Vinicius <vinicius.freitas@hsl.org.br>
2024-06-04 18:07:31 -0300

    feat: Tela de Detalhes do Hábito

--- mobile/src/screens/Home/index.tsx
+++ mobile/src/screens/Home/index.tsx
@@ -3,6 +3,7 @@ import { Header } from "../../components/Header";
 import { HabitDay, DAY_SIZE } from "../../components/HabitDay";
 
 import { generateDatesFromYearBeginning } from "../../utils/generateDatesFromYearBeginning";
+import { useNavigation } from "@react-navigation/native";
 
 const weekDays = ["D", "S", "T", "Q", "Q", "S", "S"];
 const datesFromYearStart = generateDatesFromYearBeginning();
@@ -10,6 +11,7 @@ const minimumSummaryDatesSize = 18 * 5;
 const amountOfDaysToFill = minimumSummaryDatesSize - datesFromYearStart.length;
 
 export function Home() {
+  const { navigate } = useNavigation();
   return (
     <View className="flex-1 bg-background px-8 pt-16">
       <Header />
@@ -35,7 +37,14 @@ export function Home() {
       >
         <View className="flex-row flex-wrap">
           {datesFromYearStart.map((date) => (
-            <HabitDay key={date.toISOString()} />
+            <HabitDay
+              key={date.toISOString()}
+              onPress={() => {
+                navigate("habit", {
+                  date: date.toISOString(),
+                });
+              }}
+            />
           ))}
           {amountOfDaysToFill > 0 &&
             Array.from({
